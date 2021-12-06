8427fd121ffbfeb5a93ccb8980c1c85f47cc3882
Mohammad Pooya Afshari <59111457+pooyaaf@users.noreply.github.com>
2021-12-06 11:04:17 +0330

    Create CmmCompiler.java

---
+++ phase2/CmmCompiler.java
@@ -0,0 +1,27 @@
+package main;
+
+import main.visitor.name.ASTTreePrinter;
+import main.visitor.name.ErrorVisitor;
+import parsers.*;
+import main.ast.nodes.Program;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class CmmCompiler {
+    public void compile(CharStream textStream) {
+        CmmLexer cmmLexer = new CmmLexer(textStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(cmmLexer);
+        CmmParser cmmParser = new CmmParser(tokenStream);
+
+        Program program = cmmParser.cmm().cmmProgram;
+
+        //todo
+        ErrorVisitor ErrorVisit = new ErrorVisitor();
+        ErrorVisit.visit(program);
+        
+        if(ErrorVisitor.err == false){
+            ASTTreePrinter astTreePrinter = new ASTTreePrinter();
+            astTreePrinter.visit(program);
+        }
+    }
+}
